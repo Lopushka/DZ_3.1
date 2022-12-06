2e37f71901e311d4e87b20d398581b0078b1fa11
Lopushka <sv13yat@gmail.com>
2022-12-06 22:46:21 +0300

    add new_ex_1_and_Readme

---
+++ Ex_1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("\n Введите число для проверки на палиндром: ");
+int numder = Convert.ToInt32(Console.ReadLine());
+if (numder <= 10)
+{
+    Console.Write("\n Числа-палиндромы — числа, которые в определённой позиционной системе исчисления (как правило — в десятичной) читаются одинаково как справа налево, так и слева направо");
+}
+
+else
+{
+    char[] myArray = numder.ToString().ToCharArray();
+
+    char[] newArray = myArray.Reverse().ToArray();
+
+
+    for (var i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i].Equals(newArray[i]))
+        {
+            if (i == myArray.Length - 1)
+            {
+                Console.WriteLine($"\nЧисло: {numder} является палиндромом.");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine($"\nЧисло: {numder} не является палиндромом.");
+            break;
+
+        }
+    }
+}
+
